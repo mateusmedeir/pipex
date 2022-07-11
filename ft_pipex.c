ad993c949bfa022b8896a2125de69b04e3c14f7d
Mateus Medeiros lopes <mmedeiro@c1t3s5.42.rio>
2022-07-11 14:29:48 -0300

    pipex

--- ft_pipex.c
+++ ft_pipex.c
@@ -1,20 +1,37 @@
 #include "libft/libft.h"
 #include <stdio.h>
+#include <fcntl.h>
+
+void ft_freeall(**array)
+{
+	while (*array)
+		free(*array++);
+	free(*array);
+	free(array);
+}
+
+void ft_pipex(int argc, char *argv[])
+{
+	char    *tmp;
+    char    *path;
+    char    **cmds;
+    int     value;
+    int     fd;
+
+    tmp = ft_strjoin(ft_strjoin(argv[2], " "), argv[1]);
+    cmds = ft_split(tmp, ' ');
+    free(tmp);
+    path = ft_strjoin("/bin/", cmds[0]);
+    fd = open(argv[3], O_WRONLY | O_TRUNC);
+    dup2(fd, 1);
+    value = execve(path, cmds, NULL);
+    if (value == -1)
+        perror("Could not execute execve");
+}
 
 int main(int argc, char *argv[])
 {
-	char *tmp;
-	char *path;
-	char **cmds;
-	int value;
-
-	tmp = ft_strjoin(ft_strjoin(argv[2], " "), argv[1]);
-	cmds = ft_split(tmp, ' ');
-	path = ft_strjoin("/usr/bin/", cmds[0]);
-	value = execve(path, cmds, NULL);
-	if (value == -1)
-		perror("Could not execute execve");
-	printf("Oops, something went wrong!\n");
+	ft_pipex(argc, argv);
 	return (0);
 }
 
