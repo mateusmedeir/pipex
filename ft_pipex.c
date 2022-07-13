ebfe52dfde0a0778417464d915a26d20f5438cb0
Mateus Medeiros lopes <mmedeiro@c1t3s5.42.rio>
2022-07-13 13:35:28 -0300

    adding ft_execute_cmd

--- ft_pipex.c
+++ ft_pipex.c
@@ -3,34 +3,47 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 
+void ft_execute_cmd(char *cmd[])
+{
+	char	*path;
+	int		check;
+
+	path = ft_strjoin("/bin/", cmd[0]);
+	check = execve(path, cmd, NULL);
+	if (check < 0)
+	{
+		free(path);
+		path = ft_strjoin("/usr/bin/", cmd[0]);
+		check = execve(path, cmd, NULL);
+		if (check < 0)
+			perror("could not execute execve");
+	}
+}
+
 void ft_pipex_primary(char *argv[], int *fd)
 {
 	char	*tmp;
 	char	**cmd;
-	char	*path;
 
 	tmp = ft_strjoin(ft_strjoin(argv[2], " "), argv[1]);
 	cmd = ft_split(tmp, ' ');
-	path = ft_strjoin("/bin/", cmd[0]);
 	free(tmp);
 	dup2(fd[1], 1);
 	close(fd[1]);
-	execve(path, cmd, NULL);
+	ft_execute_cmd(cmd);
 }
 
 void ft_pipex_secondary(char *argv[], int *fd)
 {
 	int		fd_file;
 	char	**cmd;
-	char	*path;
 
     cmd = ft_split(argv[3], ' ');
 	fd_file = open(argv[4], O_WRONLY | O_TRUNC);
-	path = ft_strjoin("/usr/bin/", cmd[0]);
 	dup2(fd[0], 0);
 	close(fd[0]);
 	dup2(fd_file, 1);
-	execve(path, cmd, NULL);
+	ft_execute_cmd(cmd);
 }
 
 void ft_pipex(int argc, char *argv[])
@@ -54,9 +67,7 @@ void ft_pipex(int argc, char *argv[])
 		close(fd[1]);
 		check = fork();
 		if (check == 0)
-		{
 			ft_pipex_secondary(argv, fd);
-		}
 		else
 			close(fd[0]);
 	}
