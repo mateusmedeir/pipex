4a18556dc5d370566b96da0d56f38adea930490b
Mateus Medeiros lopes <mmedeiro@c1t2s8.42.rio>
2022-07-21 12:42:04 -0300

    adding error command not found

--- ft_pipex_utils.c
+++ ft_pipex_utils.c
@@ -28,7 +28,8 @@ void ft_execute_cmd(char *cmd[], char *path, char *envp[])
 {
     if (!path)
     {
-	    perror(cmd[0]);
+		write(2, cmd[0], ft_strlen(cmd[0]));
+	    write(2, ": Command not found\n", 20);
 		exit(EXIT_FAILURE);
     }
     execve(path, cmd, envp);
@@ -43,6 +44,11 @@ void ft_pipex_primary(char *argv[], char *envp[], int *fd)
     cmd = ft_split_except(argv[2], ' ');
     path = ft_find_path(cmd[0], envp);
 	fd_file = open(argv[1], O_RDONLY);
+	if (fd_file == -1)
+    {
+        perror(argv[1]);
+        exit(EXIT_FAILURE);
+    }
     dup2(fd_file, 0);
     dup2(fd[1], 1);
     ft_execute_cmd(cmd, path, envp);
@@ -59,7 +65,12 @@ void ft_pipex_secondary(int argc, char *argv[], char *envp[], int *fd)
     cmd = ft_split_except(argv[3], ' ');
     path = ft_find_path(cmd[0], envp);
 	fd_file = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 00700);
-    dup2(fd[0], 0);
+	if (fd_file == -1)
+	{
+		perror(argv[argc - 1]);
+		exit(EXIT_FAILURE);
+	}
+	dup2(fd[0], 0);
     close(fd[0]);
     dup2(fd_file, 1);
     ft_execute_cmd(cmd, path, envp);
