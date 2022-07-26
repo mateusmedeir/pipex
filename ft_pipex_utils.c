e0a3cda572c0f50573a53beda411a908285119e1
Mateus Medeiros lopes <mmedeiro@c2t2s5.42.rio>
2022-07-26 12:54:20 -0300

    changing ft_execute_cmd

--- ft_pipex_utils.c
+++ ft_pipex_utils.c
@@ -24,8 +24,13 @@ char *ft_find_path(char *cmd, char *envp[])
     return (NULL);
 }
 
-void ft_execute_cmd(char *cmd[], char *path, char *envp[])
+void ft_execute_cmd(char *argv, char *envp[])
 {
+	char    **cmd;
+    char    *path;
+
+    cmd = ft_split_except(argv, ' ');
+    path = ft_find_path(cmd[0], envp);
     if (!path)
     {
 	    write(2, cmd[0], ft_strlen(cmd[0]));
@@ -38,11 +43,7 @@ void ft_execute_cmd(char *cmd[], char *path, char *envp[])
 void ft_pipex_primary(char *argv[], char *envp[], int *fd)
 {
     int	fd_file;
-    char    **cmd;
-    char    *path;
 
-    cmd = ft_split_except(argv[2], ' ');
-    path = ft_find_path(cmd[0], envp);
     fd_file = open(argv[1], O_RDONLY);
     if (fd_file == -1)
     {
@@ -50,20 +51,16 @@ void ft_pipex_primary(char *argv[], char *envp[], int *fd)
         exit(EXIT_FAILURE);
     }
     dup2(fd_file, 0);
+	close(fd_file);
     dup2(fd[1], 1);
-    ft_execute_cmd(cmd, path, envp);
-    close(fd_file);
-    close(fd[1]);
+	close(fd[1]);
+    ft_execute_cmd(argv[2], envp);
 }
 
 void ft_pipex_secondary(int argc, char *argv[], char *envp[], int *fd)
 {
     int     fd_file;
-    char    **cmd;
-    char    *path;
 
-    cmd = ft_split_except(argv[3], ' ');
-    path = ft_find_path(cmd[0], envp);
     fd_file = open(argv[argc - 1], O_CREAT | O_RDWR | O_TRUNC, 00700);
     if (fd_file == -1)
     {
@@ -73,5 +70,5 @@ void ft_pipex_secondary(int argc, char *argv[], char *envp[], int *fd)
     dup2(fd[0], 0);
     close(fd[0]);
     dup2(fd_file, 1);
-    ft_execute_cmd(cmd, path, envp);
+    ft_execute_cmd(argv[3], envp);
 }
